71fafde95f49e39b47c196f86818e7d33522d3ee
Aniket Yadav <aniket912yadav@gmail.com>
2024-05-30 12:44:02 +0530

    Part1 done

--- index.js
+++ index.js
@@ -1,21 +1,47 @@
-gsap.to("#box1", {
-    x: 1000,
-    duration: 2,
-    delay: 1,
-    rotate: 360,
-    backgroundColor: "blue"
-})
+// gsap.to("#box1", {
+//     x: 1000,
+//     duration: 2,
+//     delay: 1,
+//     rotate: 360,
+//     backgroundColor: "blue",
+//     repeat: -1,
+//     yoyo: true
+// })
 
-gsap.from("#box2", {
-    x: 500,
-    duration: 2,
-    delay: 1
-})
+// gsap.from("#box2", {
+//     x: 500,
+//     duration: 2,
+//     delay: 1
+// })
 
-gsap.from("h1", {
+// gsap.from("h1", {
+//     opacity: 0,
+//     duration: 2,
+//     y: 30,
+//     delay: 1,
+//     stagger: 0.4
+// })
+
+var tl = gsap.timeline()
+
+tl.from("h2", {
+    y: -20,
+    duration: 1,
+    delay: 0.5,
     opacity: 0,
-    duration: 2,
-    y: 30,
-    delay: 1,
-    stagger: 0.4
+})
+
+tl.from("h4", {
+    y: -20,
+    duration: 1,
+    opacity: 0,
+    stagger: 0.3
+
+})
+
+tl.from("h1", {
+    y: 20,
+    opacity: 0,
+    duration: 0.5,
+    scale: 0.2
 })
